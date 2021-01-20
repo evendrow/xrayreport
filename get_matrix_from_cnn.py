1bea8d1fc29142e64450048ccf5acc2edc3ef0e0
Edward Vendrow <evendrow@gmail.com>
2021-01-20 00:25:54 -0800

    Updated image processing and added torch dataset

--- get_matrix_from_cnn.py
+++ get_matrix_from_cnn.py
@@ -1,84 +1,102 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import numpy as np
+import pandas as pd
 from PIL import Image
 import torch
 from torch import nn
 from torchvision import transforms
 import torchvision.models as models
 
+import os 
+import sys
+import json
+from jsonextended import edict
 
-# In[ ]:
+CHEXPERT_PATH = "Chexpert"
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath("__file__")), CHEXPERT_PATH))
 
+from model.classifier import Classifier
 
 def load_cnn_chexpert():
-    
-    import os 
-    import sys
-    from jsonextended import edict
-    
-    initial_path = os.getcwd()
-    sys.path.append(os.path.dirname(os.path.abspath("__file__")) + "/Chexpert")
-    from model.classifier import Classifier
-    path = os.getcwd()+"/Chexpert/config/"
-    os.chdir(path)
-    with open('example.json', 'r') as f:
+    # Find the config file
+    cfg_path = os.path.join(CHEXPERT_PATH, "config/example.json")
+    with open(cfg_path, 'r') as f:
         cfg = edict.LazyLoad(json.load(f))
+    
+    # Create model and take the last convolutional layer
+    model_path = os.path.join(CHEXPERT_PATH, "config/pre_train.pth")
     model = Classifier(cfg)
-    model.load_state_dict(torch.load("pre_train.pth", map_location=torch.device('cpu')))
+    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
     modules = list(model.children())
-    modules = list(modules[0].children())[:-1]
-    model_for_use = nn.Sequential(*modules)
-    os.chdir(initial_path)
+    last_layer = list(modules[0].children())[:-1]
+    model_for_use = nn.Sequential(*last_layer)
+
     return model_for_use
 
 
-# In[2]:
+def load_cnn_densenet():
+    # load pretrained model and get last conv layer output
+    densenet = models.densenet121(pretrained=True)
+    modules = list(densenet.children())[:-1]
+    model_for_use = nn.Sequential(*modules)
+    return model_for_use
 
 
-def run_utils(model):
-    if model == "densenet121":
-        densenet = models.densenet121(pretrained=True)
-        modules = list(densenet.children())[:-1]
-        model_for_use = nn.Sequential(*modules)
-    elif model == "chexpert":
-        model_for_use = load_cnn_chexpert()
-    preprocess = transforms.Compose([
+def run_utils(model_name):
+    if model_name == "densenet121":
+        model = load_cnn_densenet();
+    elif model_name == "chexpert":
+        model = load_cnn_chexpert()
+    transform = transforms.Compose([
         transforms.Resize(256),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
-    return model_for_use, preprocess
+    return model, transform
 
 
-# In[3]:
-
-
-def make_image_representation_8_8_1024(image, model, preprocess):
-    image_dir = str(str(os.getcwd())+"/images/" + image)
+def make_image_representation_8_8_1024(data_dir, image, model, transform):
+    # Get image
+    image_dir = os.path.join(data_dir, "images/", image)
     image = Image.open(image_dir)
-    image = preprocess(image)
+    image = transform(image)
+
+    # Run image through CNN
     image_batch = image.unsqueeze(0)
-    model.eval()
     extracted_features = model(image_batch)
     np_features = extracted_features.detach().numpy()
     np_features = np_features.squeeze().transpose((1, 2, 0))
+
     return np_features
 
 
-# In[4]:
-
-
-def main_get_matrix_from_cnn(image_list, model):
+def extract_image_features(data_dir, image_list, model_name):
     matrix_list = []
-    model, preprocess = run_utils(model)
+    model, transform = run_utils(model_name)
+    
+    # set model to evaluation mode (to use running value of alpha, gamma in batchnorm)
+    model.eval()
+
     for image in image_list:
-        matrix = make_image_representation_8_8_1024(image, model, preprocess)
+        matrix = make_image_representation_8_8_1024(data_dir, image, model, transform)
         matrix_list.append(matrix)
     final_matrix = np.asarray(matrix_list)
+
     return final_matrix
+
+
+def save_feature_matrix(matrix, image_list, save_path):
+    paths = []
+    for i in range(matrix.shape[0]):
+        new_filename = os.path.basename(image_list[i]) # file.jpg
+        new_filename = os.path.splitext(new_filename)[0]+".npy" # file.npy
+        paths.append(new_filename)
+        new_path = os.path.join(save_path, new_filename)
+        np.save(new_path, matrix[i])
+
+    # save paths list to a csv file for reading by the dataset class
+    df = pd.DataFrame({"path": paths})  
+    
+    # saving the dataframe  
+    df.to_csv(os.path.join(save_path, 'paths.csv'), index=None)
+
 
---
+++ image_feature_dataset.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+import os
+
+import torch
+from torch import nn
+from torchvision import transforms
+from torch.utils.data import Dataset
+
+class ImageFeatureDataset(Dataset):
+    """Face Landmarks dataset."""
+
+    def __init__(self, csv_file, root_dir, transform=None, pooling=None, kernel_size=2):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.landmarks_frame = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.tensorTransform = transforms.ToTensor()
+        self.transform = transform
+        self.pooling = pooling
+        self.kernel_size = kernel_size
+
+    def __len__(self):
+        return len(self.landmarks_frame)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        # feature name is someothing like "123.npy"
+        feature_name = os.path.join(self.root_dir,
+                                self.landmarks_frame.iloc[idx, 0])
+        features = np.load(feature_name)
+
+        # Convert to Torch tensor
+        features = self.tensorTransform(features)
+
+        if self.pooling == "max":
+            features = nn.MaxPool2d(kernel_size, stride=kernel_size)(features)
+        elif self.pooling == "avg":
+            features = nn.AvgPool2d(kernel_size, stride=kernel_size)(features)
+
+        # Apply any given transforms
+        if self.transform:
+            features = self.transform(features)
+
+        return features
+
+
+
+
+
+
